9723c0a86c686d831c88add49378704e677f0bb2
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-06-19 16:09:47 +0400

    add

--- src/assest/js/script.js
+++ src/assest/js/script.js
@@ -3,49 +3,6 @@ import logos from "./logo.json" assert { type: "json" };
 console.log(logos);
 
 let dataBody = document.querySelector(".card-body");
-
-// for (const item of logos) {
-//   let card_button = document.createElement("button");
-//   let card_img = document.createElement("img");
-//   card_img.classList.add("logo");
-//   card_button.classList.add("card");
-//   card_button.classList.add("clickBtn");
-//   card_button.textContent = item.id;
-//   card_img.src = item.image;
-//   card_button.appendChild(card_img);
-//   dataBody.appendChild(card_button);
-// }
-
-{
-  /* <img class="logo" src="${item.image}" alt="" /> */
-}
-
-// Function to display user data
-// function showUserData(userId) {
-//   // Find the clicked user data by id
-//   const user = userData.find((user) => user.id === userId);
-
-//   // Display the user data on the screen
-//   const userInfo = document.getElementById("user-info");
-//   userInfo.innerHTML = `
-//     <h3>User Information</h3>
-//     <p><strong>Name:</strong> ${user.name}</p>
-//     <p><strong>Age:</strong> ${user.age}</p>
-//     <p><strong>Email:</strong> ${user.email}</p>
-//   `;
-// }
-
-// const clickBtn = document.getElementById("clickBtn");
-// const popup = document.getElementById("popup");
-// const closeBtn = document.getElementById("closeBtn");
-
-// clickBtn.addEventListener("click", () => {
-//   popup.style.display = "block";
-// });
-// closeBtn.addEventListener("click", () => {
-//   popup.style.display = "none";
-// });
-
 let logoContainer = document.querySelector(".card-body");
 
 logos.forEach(function (logo) {
@@ -59,6 +16,7 @@ logos.forEach(function (logo) {
     var logoDetails = getLogoDetails(logo.id);
     showLogoDetails(logoDetails);
   });
+
   logoContainer.appendChild(logoElement);
 });
 
@@ -69,7 +27,6 @@ function getLogoDetails(logoId) {
 }
 
 function showLogoDetails(logoDetails) {
-  console.log(logoDetails);
   var detailsContainer = document.querySelector(".popup-body");
   detailsContainer.details;
 
@@ -79,9 +36,11 @@ function showLogoDetails(logoDetails) {
   const popupDescription = document.querySelector(".popup-description");
   popupText.innerHTML = logoDetails.name;
   popupDescription.innerHTML = logoDetails.description;
+
 }
 
 const pop_up_header = document.querySelector(".pop-up-close");
+
 pop_up_header.addEventListener("click", function () {
   document.querySelector(".popup-body").style.display = "none";
 });
