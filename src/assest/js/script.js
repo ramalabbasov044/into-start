4e15c209ace085062d5aeddd64e116c66020b032
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-07-11 12:24:56 +0400

    add

--- src/assest/js/script.js
+++ src/assest/js/script.js
@@ -67,3 +67,25 @@ closebutton.addEventListener("click",function(){
     bg.style.backgroundColor = ""
     bg.style.opacity = "1"
 })
+
+
+
+// Animations
+
+window.addEventListener('scroll',reveal)
+
+function reveal(){
+    let reveals = document.querySelectorAll(".reveal")
+
+    for(var i = 0 ; i < reveals.length ; i++){
+      let windowheight = window.innerHeight
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 150;
+
+      if(revealTop < windowheight - revealPoint){
+          reveals[i].classList.add('active')
+      }else{
+        reveals[i].classList.remove('active')
+      }
+    }
+}
